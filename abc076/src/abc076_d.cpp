75bfb09e3c3204d65a9b66230277c2dafc07788e
ykmaku <dsm4up2c@gmail.com>
2019-10-17 01:39:20 +0900

    abc076

--- abc076/src/abc076_d.cpp
+++ abc076/src/abc076_d.cpp
@@ -1,21 +1,12 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <cstdio>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <set>
-#include <map>
-#include <numeric>
-#include <cmath>
-#include <cassert>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 typedef long long int ll;
 typedef pair<int,int> P;
 
+#define rep(i,n) for(int i=0;i<(n);++i)
+#define repi(i,a,b) for(int i=int(a);i<(b);i++)
 #define all(x) x.begin(),x.end()
 
 const ll mod = 1e9+7;
@@ -24,40 +15,53 @@ const ll INF = 1e9;
 ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
 int dx[4]={1,0,-1,0};
 int dy[4]={0,1,0,-1};
+bool valid(int lx,int ux,int ly,int uy,int x,int y){
+	return lx<=x&&x<ux&&ly<=y&&y<uy; 
+}
+
+double f(double l,double r,double v,double x){
+	if(x<=l) return v+(l-x);
+	if(l<x&&x<=r)return v;
+	return v+r+x;
+}
 
 int main()
 {
 	int n;
 	cin>>n;
-	// vector<int>t(n);
-	vector<double> t(n+1,0),v(n+1,0);
-	for (int i = 1; i <= n; i++){
-		cin>>t[i];
-		// t[i] *= 2;
-	}
-	for (int i = 1; i <= n; i++){
-		cin>>v[i];
-		// v[i]*=2;
-	}
+	vector<double> t(n+1,0),v(n);
+	repi(i,1,n+1)cin>>t[i];
+	rep(i,n)cin>>v[i];
 
-	vector<double> p(2*(n+1),0);
-	for (int i = 0; i < n; i++){
-		if(v[i]-v[i+1] >= t[i+1]-t[i]){
-			p[i+1] = (v[i]+v[i+1])/2;
-			p[i+2] = v[i+1];
-		}else{
-			double a = v[i]-t[i];
-			double b = t[i+1] + v[i+1];
-			p[i+1] = (b-a)/2;
-			p[i+2] = v[i+1];
+	rep(i,n)t[i+1]+=t[i];
+	int T=t[n];
+
+	vector<double> speed(2*T+1,0);
+	
+	double time=0.5;
+	repi(i,1,2*T){
+		speed[i]=speed[i-1]+0.5;
+		rep(j,n){
+			speed[i]=min(speed[i],f(t[j],t[j+1],v[j],time));
 		}
+		time+=0.5;
 	}
-	double ans = 0;
-	for (int i = 0; i < 2*n; i++){
-		ans += 0.25*(p[i]+p[i+1]);
+	time=T-0.5;
+	for(int i = 2*T-1; i >0; i--){
+		rep(j,n){
+			speed[i]=min(speed[i],speed[i+1]+0.5);
+		}
+		time-=0.5;
 	}
-	for(auto pp:p)cout<<pp<<" ";
-	cout<<endl;
-	cout<<ans<<endl;
+	
+	// for(auto s:speed)cout<<s<<" ";
+	// cout<<endl;
+
+	double ans=0;
+	rep(i,2*T){
+		ans += 0.25*(speed[i]+speed[i+1]);
+	}
+	cout<<setprecision(10)<<ans<<endl;
+
 	return 0;
 }
